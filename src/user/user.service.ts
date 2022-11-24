489e87bff06483e38c484a621272c5f92eac3b1b
Simone Traversi <simone.traversi@makrshakr.com>
2022-11-24 11:53:16 +0100

    little check in users service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -24,11 +24,26 @@ export class UserService {
     return this.userRepository.findOneBy({ email });
   }
 
-  createUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto) {
+    const user = await this.getUserByEmail(createUserDto.email);
+    if (user) {
+      throw new HttpException(
+        `User with email ${createUserDto.email} is already exists`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     return this.userRepository.save(createUserDto);
   }
 
-  updateUser(updateUserDto: UpdateUserDto, userId: number) {
+  async updateUser(updateUserDto: UpdateUserDto, userId: number) {
+    const user = await this.getUserByEmail(updateUserDto.email);
+    if (user) {
+      throw new HttpException(
+        `User with email ${updateUserDto.email} is already exists`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     return this.userRepository.update(userId, updateUserDto);
   }
 
